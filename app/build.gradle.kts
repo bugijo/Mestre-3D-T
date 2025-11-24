0ea7075e6be8f1964a78d61b9cbe0abbada003ff
bugijo <gil.jonathan.pereira@gmail.com>
2025-11-24 17:33:49 -0300

    Fix: Restauradas todas as dependências do Material3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,6 +88,7 @@ dependencies {
     implementation(composeBom)
     androidTestImplementation(composeBom)
 
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
