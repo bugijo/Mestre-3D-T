cd8b81ccc03f90c2392a0b8b8b990632b10429dc
bugijo <gil.jonathan.pereira@gmail.com>
2025-12-14 18:43:03 -0300

    chore: remove legacy android project files

--- delete_extra_braces.ps1
+++
@@ -1,4 +0,0 @@
-$content = Get-Content 'app\src\main\java\com\mestre3dt\ui\screens\SessionCockpitScreen.kt' -Encoding UTF8
-$newContent = $content[0..560] + $content[563..($content.Length-1)]
-$newContent | Set-Content 'app\src\main\java\com\mestre3dt\ui\screens\SessionCockpitScreen.kt' -Encoding UTF8
-Write-Host "Deletadas linhas 561-562 com sucesso!"
